54826b604a67c4016a0e932a29f34655a1fe93a3
Max <54718475+Fuhrer13@users.noreply.github.com>
2020-06-02 21:49:26 +0300

    Updates

---
+++ precache-manifest.eb85b97b667cd51f0215486353059edd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbcabd214177c1188e3984e31bfadb8",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "5716a851d4c4577a5a77",
+    "url": "/goit-react-hw-05-phonebook/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "f638378eb14ddc5e2dd0",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.33f76487.chunk.css"
+  },
+  {
+    "revision": "5716a851d4c4577a5a77",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.698442b3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.698442b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f638378eb14ddc5e2dd0",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.a4a1a3a6.chunk.js"
+  },
+  {
+    "revision": "d11d900b24334f5901a5",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.2b224eb8.js"
+  }
+]);
